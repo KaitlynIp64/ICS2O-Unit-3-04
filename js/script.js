7a0f57590ff7b3c6440598f0f42879c530311c72
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-06 17:16:36 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 // Copyright (c) 2022 Kaitlyn Ip All rights reserved
 //
 // Created by: Kaitlyn Ip
-// Created on: Mar 2022
-// This file contains xxx
+// Created on: Apr 2022
+// This file contains the JS functions for index.html
 
+'use strict'
 /**
- * This function xxx.
+ * This function calculates celsius from Fahrenheit.
  */
+function calculate () {
+  // input
+  const fahrenheit = parseFloat(document.getElementById("fahrenheit").value)
+
+  // process
+  const celsius = (fahrenheit - 32) x 5.0/9.0
+
+  // output
+  document.getElementById('celsius').innerHTML = 'celsius is: ' + celsius.toFixed(2) + ' °C'
+}
